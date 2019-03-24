f18ae9352a5077a2b19b1a5b3933702520e08899
Carl Cortright <carl@carlcortright.com>
2019-03-23 19:51:55 -0600

    add heroku ingo

--- ClassQAAPI/settings.py
+++ ClassQAAPI/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import django_heroku
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MUST GO AT BOTTOM
+django_heroku.settings(locals())
